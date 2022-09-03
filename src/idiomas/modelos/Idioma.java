a44b7636c2964fe7f10b61a34009956f6156ff74
MaximilianoPaz <maxipaz0047@gmail.com>
2022-09-03 12:34:47 -0300

    comienzo tp1

---
+++ src/idiomas/modelos/Idioma.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package idiomas.modelos;
+
+/**
+ *
+ * @author Ana Florencia Paz
+ */
+public class Idioma {
+    String nombre;
+}
---
+++ src/principal/controladores/ControladorPrincipal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal.controladores;
+
+/**
+ *
+ * @author Ana Florencia Paz
+ */
+public class ControladorPrincipal {
+    public static void main(String[] args) {
+        ArrayList
+    }
+}
