607708d9c2046625fa118419bfc29abf8cced235
MaximilianoPaz <maxipaz0047@gmail.com>
2022-09-16 23:27:31 -0300

    cambios en tp2

---
+++ src/autores/modelos/Profesor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autores.modelos;
+
+/**
+ *
+ * @author Ana Florencia Paz
+ */
+
+public class Profesores {
+    String nombre,apellido,cx,cargo,clave;
+    int DNI;
+}
